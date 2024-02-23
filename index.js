19dd802194adb6af0716d7405d87cacab74b3abc
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-02-22 22:29:01 -0300

    Novo Design

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-const carousel = document.querySelector('.carousel')
+const carousel = document.querySelectorAll('.carrosel .product')
+const leftBtn = document.querySelector('.left')
+const rigthBtn = document.querySelector('.rigth')
+let carousel2 = document.querySelector('.carrosel')
+let count = 0
 
-let descer = false
-
-const start = () => {
-  descer = true
-}
-
-const mover = (e) => {
-  if(!descer) return
-  carousel.scrollLeft = e.pageX
-
-}
-
-carousel.addEventListener('mousedown', start)
-carousel.addEventListener('mousemove', mover)
 
 const bc = [
   {
@@ -35,6 +25,56 @@ const bc = [
     preco: 27
   },
 ]
+ 
+function left(){
+
+  if (count === 0){
+    count = carousel.length / 3 - 1
+  } else {
+    count--
+  }
+  mover()
+}
+
+function renderProduct(product) {
+  const productDiv = document.createElement('div');
+  productDiv.classList.add('product');
+  productDiv.innerHTML = `
+    <div class="imagemProduct">
+      <img src="${product.img}" alt="">
+    </div>
+    <div class="inf">
+      <h3 class="title">${product.h1}</h3>
+      <span class="price">${product.preco}</span>
+      <button>Adicionar</button>
+    </div>
+  `;
+  return productDiv;
+}
+
+function rigth(){
+
+  count--;
+  if (count < 0) {
+    count = bc.length - 1;
+  }
+  mover()
+
+  const productDiv = renderProduct(bc[0]);
+  carousel2.appendChild(productDiv);
+}
+
+function mover(){
+ 
+  carousel.forEach((item) => {
+    item.style.transform = `translateX(${count * 103}px)`
+  })
+}
+
+leftBtn.addEventListener('click', left)
+rigthBtn.addEventListener('click', rigth)
+
+
  
 
 function mostrarNav(){
